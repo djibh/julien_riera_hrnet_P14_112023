d2f9d795d59e221ca2e048492f39a92d2e07a4b8
Djibh <julien.riera@protonmail.com>
2023-11-11 15:54:58 +0100

    refactor (HOME) > Create <Header/>

--- src/components/employees/EmployeesTable.tsx
+++ src/components/employees/EmployeesTable.tsx
@@ -7,9 +7,8 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
-import AddCircleIcon from '@mui/icons-material/AddCircle';
-import SearchIcon from '@mui/icons-material/Search';
-import SelectDropdown from '../reusable/SelectDropdown'
+import Header from '../header/Header'
+
 import { columns, rows } from './DataTableConfig'
 
 import styled from 'styled-components';
@@ -30,15 +29,7 @@ export default function EmployeesTable() {
     };
   return (
     <TableWrapperStyled className='wrapper'>
-      <h1>EMPLOYEES</h1>
-      <div className='search'>
-        <div className='search-input__container'>
-          <SearchIcon />
-          <input type="text" id='search-input'/>
-        </div>
-        <SelectDropdown label='Sort' options={['First name','Last name']} />
-        <AddCircleIcon id='add-button'/>
-      </div>
+      <Header />
       <Paper sx={{ width: '100%', overflow: 'hidden', backgroundColor:'#444',borderRadius:'10px', padding: '0.5em 1.5em' }}>
           <TableContainer sx={{ maxHeight: 500 }}>
             <Table stickyHeader aria-label="sticky table">
@@ -96,44 +87,4 @@ const TableWrapperStyled = styled.div`
     max-width: 1200px;
     margin: auto;
     text-align: center;
-
-    svg {
-      font-size: 2rem;
-    }
-
-    h1 {
-      font-size: 2rem;
-      margin: 1em;
-    }
-
-    .search {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      gap: 0.5em;
-      width: 100%;
-      margin-bottom: 1em;
-    }
-
-    .search-input__container {
-      display: flex;
-      padding: 0.5em;
-      background-color: rgba(68, 68, 68, 0.35);
-      backdrop-filter: blur(3px);
-      -webkit-backdrop-filter: blur(3px);
-      border-radius: 10px;
-      flex: 1;
-    }
-
-    #search-input {
-      flex: 1;
-      outline: none;
-      border: none;
-      font-size: 1.2rem;
-    }
-
-    #add-button {
-      font-size: 2rem;
-      filter: drop-shadow(0px 1px 2px black)
-    }    
 `;
